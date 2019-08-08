e45ff78cdb0cc60fedc775e543a2a453fa520cd0
hanhan <hanhanwu@users.noreply.github.com>
2019-08-08 15:44:08 -0700

    Update sklearn_notes.py

--- Prototype_Commonly_Used_Code/sklearn_notes.py
+++ Prototype_Commonly_Used_Code/sklearn_notes.py
@@ -1,4 +1,5 @@
 # check metrics strings can be used in "scoring" param
 ## NOTE: `from sklearn.metrics import` does not related to the strings used in `scoring`
+# https://scikit-learn.org/stable/modules/model_evaluation.html
 import sklearn
 sorted(sklearn.metrics.SCORERS.keys())
